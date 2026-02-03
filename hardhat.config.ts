f9a29f5a95700ef5f38ef43ed2bfb1af17143fa1
Gerald Nqobile Macintosh Ndlovu <ndlovugerald707@gmail.com>
2026-02-03 17:48:57 +0800

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,1 +1,11 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-ethers";
 
+const config: HardhatUserConfig = {
+  solidity: { compilers: [{ version: "0.8.17", settings: {} }] },
+  networks: {
+    hardhat: {},
+  },
+};
+
+export default config;
